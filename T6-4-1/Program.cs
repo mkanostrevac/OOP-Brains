d7e88074d18064844c4c39a7ef4d5707e12b320f
stawross <mkanostrevac@gmail.com>
2018-03-21 21:29:15 +0100

    T6 Added Exceptions

---
+++ T6-4-1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] M = new int[][] 
+            {
+                new int [] { 1, 2},
+                new int [] { 3, 4}
+            };
+
+            Console.WriteLine(IzracunajDeterminantu(M));
+
+            M = new int[][]
+            {
+                new int [] { 1 }
+            };
+
+            Console.WriteLine(IzracunajDeterminantu(M));
+            Console.WriteLine(IzracunajDeterminantu(null));
+            Console.ReadKey();
+        }
+
+        static double IzracunajDeterminantu(int [][] M)
+        {
+            try
+            {
+                return M[0][0] * M[1][1] - M[0][1] * M[1][0];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("M nije ispravne velicine!");
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("M ne postoji!");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ T6-4-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Koren(2, 3, -4));
+            Console.WriteLine(Koren(0, 3, 4));
+            Console.WriteLine(Koren(2, 3, 4));
+            Console.ReadKey();
+        }
+        
+        static double Koren(double A, double B, double C)
+        {
+            if (A == 0)
+            {
+                throw new ArgumentException("A ne moze biti nula!");
+            }
+            else
+            {
+                double D = B * B - 4 * A * C;
+
+                if (D <  0)
+                {
+                    throw new InvalidOperationException("Diskriminanta manja od nule!");
+                }
+
+                return (-B + Math.Sqrt(D)) / (2 * A);
+            }
+        }
+    }
+}
