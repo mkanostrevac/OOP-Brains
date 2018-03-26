96907fd584de9d776babb568ba814d3f9b3f8579
stawross <mkanostrevac@gmail.com>
2018-03-27 00:34:24 +0200

    Updated 5.2

---
+++ T7-5-2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T7_5_2
+{
+    public delegate void IzvrsilacOperacija(int n);
+
+    public class Program
+    {
+        static private int rezultat;
+
+        static void Main(string[] args)
+        {
+            rezultat = 0;
+            IzvrsilacOperacija digitron = new IzvrsilacOperacija(Sabiranje);
+
+            digitron.Invoke(1);
+            Console.WriteLine(rezultat);
+            digitron.Invoke(2);
+            Console.WriteLine(rezultat);
+            digitron.Invoke(3);
+            Console.WriteLine(rezultat);
+
+            digitron += new IzvrsilacOperacija(Mnozenje);
+            digitron.Invoke(2); // izvrsavanje vise operacija u isto vreme (izvrsava se i sabiranje i mnozenje)
+            Console.WriteLine(rezultat);
+
+            digitron -= new IzvrsilacOperacija(Mnozenje);
+            digitron.Invoke(3);
+            Console.WriteLine(rezultat);
+
+            Console.ReadKey();
+        }
+
+        static void Sabiranje(int broj)
+        {
+            rezultat += broj;
+        }
+
+        static void Mnozenje(int broj)
+        {
+            rezultat *= broj;
+        }
+    }
+}
