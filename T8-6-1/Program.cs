e44b50ae5919f9bc0b16b1b2031837e6460c0de8
stawross <mkanostrevac@gmail.com>
2018-03-28 18:35:51 +0200

    Updated Singleton

---
+++ T8-6-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T8_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Singleton s = new Singleton();
+            // 'Singleton.Singleton()' is inaccessible due to its protection level
+
+            Singleton.Instance.Ispis("Pozivanje metode prvi put");
+            Singleton.Instance.Ispis("Pozivanje metode drugi put");
+        }
+    }
+}
---
+++ T8-6-1/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T8_6_1
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+        
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+
+                return instance;
+            }
+        }
+
+        public void Ispis(string poruka)
+        {
+            Console.WriteLine(poruka);
+        }
+    }
+}
